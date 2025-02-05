103994d6c499c33c73cac9d7b06a439c62a7f03a
ko-tisha <ko.tisha@list.ru>
2025-02-05 11:30:05 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function changeStyle(button) {
+
+    if (button.style.backgroundColor === "#D3217E") {
+        button.style.backgroundColor = "#FF68AA";
+        button.style.margin = "20px";
+
+    } else {
+        button.style.backgroundColor = "#D3217E";
+        button.style.margin = "10px";
+    }
+}
